d9a63b2230905ea98cd2269037e005d38340c60d
shivabalan1712 <shivabalan2002@gmail.com>
2025-08-09 16:10:39 +0530

    fixed mapping error

--- src/main/java/com/tngovschools/admission_system/dto/StudentDTO.java
+++ src/main/java/com/tngovschools/admission_system/dto/StudentDTO.java
@@ -1,14 +1,14 @@
-//package com.tngovschools.admission_system.dto;
-//
-//import lombok.Data;
-//
-//@Data
-//public class StudentDTO {
-//    private String name;
-//    private String fatherName;
-//    private String standard;
-//    private String section;
-//    private String email;
-//    private String phone;
-//    private String address;
-//}
+package com.tngovschools.admission_system.dto;
+
+import lombok.Data;
+
+@Data
+public class StudentDTO {
+    private String name;
+    private String fatherName;
+    private String standard;
+    private String section;
+    private String email;
+    private String phone;
+    private String address;
+}
